5b97846fef9d1753ddc9416277b339541d22b0b0
Phat <phatth1203@gmail.com>
2019-09-20 18:10:50 +0700

    buon

--- searcher/services.py
+++ searcher/services.py
@@ -1,22 +1,26 @@
 from post.models import Post
 from helper import common as commonHelper
 from .models import Searcher, Keywords, Vocabulary, VectorSpace
+from django.db.models import Q
 
 
 def search_result(query):
-    docs = Post.objects.all()
-    vocab = Vocabulary.objects.all()[0].get()
-    searcher = Searcher(docs, vocab)
     terms = commonHelper.cleaned_text(commonHelper.norm_text(query))
+    dj_query = get_query(terms)
+    posts = Post.objects.filter(dj_query)
+
+    vocab = Vocabulary.objects.all()[0].get()
+    searcher = Searcher(posts, vocab)
+
     doc_ids = searcher.search(terms)
-    docs = Post.objects.filter(pk__in=doc_ids)
+    posts = Post.objects.filter(pk__in=doc_ids)
 
     upsert_keywords(terms)
     # nothing found mark keywords not in collection
-    # if len(docs) == 0:
+    # if len(posts) == 0:
     #     upsert_keywords(terms, 0)
 
-    return get_results(docs, doc_ids, terms)
+    return get_results(posts, doc_ids, terms)
     
 
 def get_results(docs, ids, terms):
@@ -42,3 +46,17 @@ def upsert_keywords(terms):
         if not created:
             keyword.num_of_searches += 1
             keyword.save()
+
+
+def get_query(terms):
+    if not terms:
+        return
+
+    is_contain = lambda term: Q(title__icontains=term) | \
+                        Q(content__icontains=term) | \
+                        Q(salary_range__icontains=term)
+
+    condition = is_contain(terms[0])
+    for term in terms[1:]:
+        condition = condition | is_contain(term)
+    return condition
