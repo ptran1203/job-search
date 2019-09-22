f86150f5bc841b102cf4ea8415fefebd06b01a38
ptran1203 <phatth1203@gmail.com>
2019-09-22 21:20:01 +0700

    prod

--- scheduler/core.py
+++ scheduler/core.py
@@ -3,7 +3,7 @@ import requests
 import subprocess
 
 SIX_HOURS = 21600
-HOST_URL = "http://localhost:8000/"
+HOST_URL = "http://jsearcher-api.herokuapp.com/"
 
 
 def set_interval(func, sec):
@@ -16,15 +16,16 @@ def set_interval(func, sec):
 
 # use later
 def ping():
-    r = requests.get(HOST_URL)
+    r = requests.get(HOST_URL + 'api/keywords')
 
 def run_spider():
-    subprocess.call(['python3', 'spider/core.py'])
+    subprocess.call(['python', 'spider/core.py'])
 
 def build_vectorspace():
     r = requests.get(HOST_URL + 'api/vectorspace')
 
 # export
-def start_scheduler(time_interval=SIX_HOURS):
+def start_scheduler(time_interval=SIX_HOURS/2):
     set_interval(run_spider, time_interval)
-    set_interval(build_vectorspace, time_interval + SIX_HOURS/2)
+    set_interval(build_vectorspace, time_interval + 6000)
+    set_interval(ping, 2500)
