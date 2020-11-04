2ccb0068dafa8b20e8b5998a4a1f3a1c70607d24
phattran <phat.tranhoang@cyberlogitec.com>
2020-11-04 16:47:56 +0700

    changepath

--- salary_estimation/word2vec.py
+++ salary_estimation/word2vec.py
@@ -4,7 +4,9 @@ import numpy as np
 
 class Embedding:
     def __init__(self):
-        self.model = gensim.models.Word2Vec.load("./storage/word2vec_model")
+        self.model = gensim.models.Word2Vec.load(
+            "./salary_estimation/storage/word2vec_model"
+        )
 
     def word2vec(self, word):
         if word in self.model.wv.vocab:
